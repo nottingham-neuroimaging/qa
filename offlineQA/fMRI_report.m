2acf5af603d49303e76d7ab046796e8e629efb56
Kevin Aquino <aquino@physics.usyd.edu.au>
2015-11-19 10:26:30 +0000

    Adding in files for offline QA

---
+++ offlineQA/fMRI_report.m
@@ -0,0 +1,9 @@
+% This function here is to calculate the tSNR, image SNR then generate a
+% report showing these if needed. A work in progress.
+
+function out = fMRI_report(filenames,parameters)
+
+
+
+
+end
