52d4dc8a436112f8b50e4862b0e4bad645f9802c
Kevin Aquino <aquino@physics.usyd.edu.au>
2015-11-24 11:46:52 +0000

    This sets intial Parameters for the GUI

---
+++ offlineQA/setInitialParams.m
@@ -0,0 +1,23 @@
+% Here is a function to obtain default parameters
+function scanParams = setInitialParams(filenames)
+
+% initialize the array.
+scanParams = struct;
+% get intial parameters from the
+
+
+for nf=1:length(filenames)
+    hdr = cbiReadNiftiHeader(filenames{nf});
+    scanParams(nf).fileName = filenames{nf};
+    scanParams(nf).dynNOISEscan = 0;
+    scanParams(nf).volumeSelect = [hdr.dim(5)];
+    scanParams(nf).outputBaseName = hdr.hdr_name(1:end-4);
+    scanParams(nf).slices = 1:hdr.dim(4);
+    scanParams(nf).ROI_box = [];
+    % Some of the characters in this contain non-unicode characters, so
+    % remove them here. 
+    unicodenotes = unicode2native(hdr.descrip);
+    scanParams(nf).notes = hdr.descrip(setdiff(1:length(unicodenotes),find(~unicodenotes)));    
+end
+
+end
