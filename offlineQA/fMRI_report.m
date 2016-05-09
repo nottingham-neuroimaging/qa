1e3bed041c57b461801298c600889a23d82941a4
Julien Besle <julienbesle@gmail.com>
2016-05-09 11:36:23 +0100

    fMRI_report.m : made it work if starting from different folder (simply cd into folder wehre files ahve been selected)

--- offlineQA/fMRI_report.m
+++ offlineQA/fMRI_report.m
@@ -3,17 +3,20 @@
 
 % function [tSNR_ROI,iSNR]=fMRI_report()
 
+
+% Select the the scans taht we want to use.
+[filenames,pathname] = uigetfile({'*.img;*.nii;*.nii.gz','Nifti/Analyze files'},'Select the scans you want a report on... ', 'multiselect', 'on');
+if ischar(filenames)
+  filenames = {filenames};
+end
+
 % Make a folder called QA_report, where the tSNR maps are calculated, and
 % where the summary file will live once this is run.
+cd(pathname);
 if(~isdir('QA_report'));
     mkdir('QA_report');
 end
 
-% Select the the scans taht we want to use.
-[filenames] = uigetfile({'*.img;*.nii;*.nii.gz','Nifti/Analyze files'},'Select the scans you want a report on... ', 'multiselect', 'on');
-if ischar(filenames)
-  filenames = {filenames};
-end
 % intialize intial parameters from the nifti headers.
 scanParams = setInitialParams(filenames);
 
