9ff6d6555e7dad29160e4d3c1d2ad78fe7830115
Kevin Aquino <aquino@physics.usyd.edu.au>
2016-05-09 09:52:37 +0100

    added css tags

--- offlineQA/generateHTMLReport.m
+++ offlineQA/generateHTMLReport.m
@@ -6,14 +6,22 @@ function generateHTMLReport(scanParams)
 fileID = fopen('QA_report/index.html','w');
 
 % Make the header first
+[path,file] = fileparts(which('generateHTMLReport.m'));
+copyfile([path filesep 'websiteStyle.css'],'QA_report/')
 
-fprintf(fileID,'%s','<html><title>QA Report</title><h1>Quality Assurance report: </h1>');
+
+fprintf(fileID,'%s','<html><title>QA Report</title>');
+fprintf(fileID,'%s','<head><link rel="stylesheet" href="websiteStyle.css"></head><div id="header"><h1>Quality Assurance report: </h1><div id="navigation"><a href="index.html">Summary</a></div></div>');
+fprintf(fileID,'\n');
+fprintf(fileID,'%s','<div id="content">');
 % Make the table:
 fprintf(fileID,'%s','<table><tr><td width=300px><a href="tSNR.html"><h2>tSNR report</h2></a></td>');
 fprintf(fileID,'%s',['<td><a href="tSNR.html"><img src=' scanParams(1).fileName(1:end-4) '_tSNR_IMAGE.png width=200></a></td></tr>']);
 fprintf(fileID,'\n');
 fprintf(fileID,'%s','<table><tr><td width=300px><a href="mean.html"><h2>Mean Images</h2></a></td>');
 fprintf(fileID,'%s',['<td><a href="mean.html"><img src=' scanParams(1).fileName(1:end-4) '_Mean_IMAGE.png width=200></a></td></tr>']);
+fprintf(fileID,'\n');
+fprintf(fileID,'%s','</div>');
 fprintf(fileID,'%s','</table>');
 fprintf(fileID,'%s','</html>');
 
@@ -24,9 +32,12 @@ fclose(fileID);
 fileID = fopen('QA_report/tSNR.html','w');
 
 % Make the header first
+fprintf(fileID,'%s','<html>');%<h1>SCAN: </h1>');
+fprintf(fileID,'%s','<head><link rel="stylesheet" href="websiteStyle.css"></head><div id="header"><h1>Quality Assurance report: </h1><div id="navigation"><a href="index.html">Summary</a></div></div>');
+fprintf(fileID,'\n');
+fprintf(fileID,'%s','<div id="content">');
 
-fprintf(fileID,'%s','<html><title>tSNR report</title><h1>SCAN: </h1>');
-
+fprintf(fileID,'%s','<br><h2>tSNR</h2>');
 
 for nf=1:length(scanParams)
     fprintf(fileID,'%s',['<h2>' scanParams(nf).fileName '</h2><br>']);
@@ -35,6 +46,8 @@ for nf=1:length(scanParams)
     fprintf(fileID,'%s',['<img src=cbar.png width=400><br>']);
     fprintf(fileID,'%s',['Notes: ' scanParams(nf).notes '<br><br><br>']);
 end
+
+fprintf(fileID,'%s','</div>');
 
 fprintf(fileID,'%s','</html>');
 
@@ -45,7 +58,11 @@ fclose(fileID);
 
 fileID = fopen('QA_report/mean.html','w');
 % Make the header first
-fprintf(fileID,'%s','<html><title>Mean Images</title><h1>SCAN: </h1>');
+fprintf(fileID,'%s','<html><title>Mean Images</title>');
+fprintf(fileID,'%s','<head><link rel="stylesheet" href="websiteStyle.css"></head><div id="header"><h1>Quality Assurance report: </h1><div id="navigation"><a href="index.html">Summary</a></div></div>');
+fprintf(fileID,'\n');
+fprintf(fileID,'%s','<div id="content">');
+fprintf(fileID,'%s','<br><h2>Mean Images</h2>');
 
 for nf=1:length(scanParams)
     fprintf(fileID,'%s',['<h2>' scanParams(nf).fileName '</h2><br>']);
@@ -53,7 +70,7 @@ for nf=1:length(scanParams)
         '<img src=' scanParams(nf).fileName(1:end-4) '_Mean_IMAGE.png width=800></a> <br>' ]);    
     fprintf(fileID,'%s',['Notes: ' scanParams(nf).notes '<br><br><br>']);
 end
-
+fprintf(fileID,'%s','</div>');
 fprintf(fileID,'%s','</html>');
 fclose(fileID);
 
