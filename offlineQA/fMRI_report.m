9d76f94b8acef670ed787811a064a5925219f679
Kevin Aquino <aquino@physics.usyd.edu.au>
2016-06-13 09:51:44 +0100

    Slowly making this into a function

--- offlineQA/fMRI_report.m
+++ offlineQA/fMRI_report.m
@@ -1,27 +1,28 @@
 % This script here is to calculate the tSNR, image SNR then generate a
-% report showing these if needed. A work in progress ...
+% report showing these if needed. 
 
-% function [tSNR_ROI,iSNR]=fMRI_report()
+% function fMRI_report()
 
 
-% Select the the scans that we want to use.
-[filenames,pathname] = uigetfile({'*.img;*.nii;*.nii.gz','Nifti/Analyze files'},'Select the scans you want a report on... ', 'multiselect', 'on');
-if ischar(filenames)
-  filenames = {filenames};
-elseif isnumeric(filenames)
-  return;
-end
+	% Select the the scans that we want to use.
+	[filenames,pathname] = uigetfile({'*.img;*.nii;*.nii.gz','Nifti/Analyze files'},'Select the scans you want a report on... ', 'multiselect', 'on');
+	if ischar(filenames)
+	  filenames = {filenames};
+	elseif isnumeric(filenames)
+	  return;
+	end
 
-% Make a folder called QA_report, where the tSNR maps are calculated, and
-% where the summary file will live once this is run.
-cd(pathname);
-if(~isdir('QA_report'));
-    mkdir('QA_report');
-end
+	% Make a folder called QA_report, where the tSNR maps are calculated, and
+	% where the summary file will live once this is run.
+	cd(pathname);
+	if(~isdir('QA_report'));
+	    mkdir('QA_report');
+	end
 
-% intialize intial parameters from the nifti headers.
-scanParams = setInitialParams(filenames);
+	% intialize intial parameters from the nifti headers.
+	scanParams = setInitialParams(filenames);
 
-% Now run the GUI.
-scanParams = fMRI_report_GUI(scanParams);
+	% Now run the GUI.
+	scanParams = fMRI_report_GUI(scanParams);
 
+% end
