105c24b78e864ac5d3316df1df10df24e5943c48
ppxma7 <ppxma7@nottingham.ac.uk>
2016-08-12 11:33:29 +0100

    adds help

--- offlineQA/tSNR_dyn.m
+++ offlineQA/tSNR_dyn.m
@@ -1,6 +1,16 @@
 function [] = tSNR_dyn(dataFilename, varargin)
 %tSNR_dyn - easy way to quickly get tSNR for different dynamic length
-% 
+%         - call it like:
+%         - tSNR_dyn([], 'croppedTimeSeries=[1 20]'
+%         - Where [1 20] specified the dynamic range  
+%         - This is all possible to do with the function tSNR, but
+%         difficult to get it to print info out, so I made this.
+%         - K. Aquino's qa function only allows a slice selection, not a
+%         dynamic selection (through the GUI at least).
+%
+% ma 2016/08/12 
+%
+% see also tSNR, tSNR_report, cbiReadNifti, varargin
 
 validInputArgs = {'cropTimeSeries'};
 eval(evalargs(varargin, validInputArgs))
