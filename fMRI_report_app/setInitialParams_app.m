94ff692d3713a3ac76ba4eb04527cf3f43f9bda5
Michael Asghar <michael.asghar@nottingham.ac.uk>
2020-02-26 15:49:55 +0000

    prettier app version

---
+++ fMRI_report_app/setInitialParams_app.m
@@ -0,0 +1,28 @@
+% Here is a function to obtain default parameters
+function scanParams = setInitialParams_app(filenames)
+
+% initialize the array.
+scanParams = struct;
+% get intial parameters from the
+
+
+for nf=1:length(filenames)
+    hdr = cbiReadNiftiHeader(filenames{nf});
+    scanParams(nf).fileName = filenames{nf};
+    scanParams(nf).dynNOISEscan = 0;
+    scanParams(nf).volumeSelect = [hdr.dim(5)];
+    scanParams(nf).volumeSelectFirst = [hdr.dim(5)/hdr.dim(5)];
+    scanParams(nf).sliceSelectFirst = 1;
+    scanParams(nf).sliceSelectLast = [hdr.dim(4)];
+    scanParams(nf).outputBaseName = hdr.hdr_name(1:end-4);
+    scanParams(nf).slices = 1:hdr.dim(4);
+    scanParams(nf).dims = hdr.dim(2:4);
+    scanParams(nf).ROI_box = [];
+    % Some of the characters in this contain non-unicode characters, so
+    % remove them here. 
+    unicodenotes = unicode2native(hdr.descrip);
+    scanParams(nf).notes = hdr.descrip(setdiff(1:length(unicodenotes),find(~unicodenotes)));
+    scanParams(nf).orientation = 3;
+end
+
+end
