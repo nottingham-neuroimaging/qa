53453adb1777b67819cd4cc788f5aa9877aa6678
Michael Asghar <michael.asghar@nottingham.ac.uk>
2021-04-01 12:55:59 +0100

    MRIread instead of cbiwritenifit

--- fMRI_report_app/generateSliceSummary_app.m
+++ fMRI_report_app/generateSliceSummary_app.m
@@ -37,7 +37,9 @@ if(nargin<8)
     orientation = 3;
 end
 
-data = cbiReadNifti(filename);
+%data = cbiReadNifti(filename);
+dataTmp = MRIread(filename);
+data = dataTmp.vol;
 data = mean(data,4);
 % This here changes the image orientation if you need to..
 switch orientation
